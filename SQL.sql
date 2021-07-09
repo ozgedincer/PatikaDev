a864ab185346b91b4dd5b3acfc125641f21fef5d
Özge Dinçer <67578643+ozgedincer@users.noreply.github.com>
2021-07-09 21:35:28 +0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,5 @@
+-- ÖDEV 1
+
 --film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 SELECT title, description FROM film ;
 --film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.

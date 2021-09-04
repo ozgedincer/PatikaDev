f1d0075db715e2f9b17a7f8fb3bb1f7f9f9c8494
Özge Dinçer <dncerozge@gmail.com>
2021-09-04 22:05:52 +0300

    ödev7

--- SQL.sql
+++ SQL.sql
@@ -92,3 +92,24 @@ WHERE rental_rate = 0.99 ;
 --film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 SELECT COUNT(DISTINCT replacement_cost) FROM film 
 WHERE length > 150 ;
+
+
+--ÖDEV 7
+
+--film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT rating, COUNT(*) FROM film 
+GROUP BY rating ;
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda 
+--film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost 
+HAVING  COUNT(*) > 50 ;
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id ;
+--city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra 
+--en fazla şehir sayısı barındıra country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC 
+LIMIT 1 ;

c5f82bcd8eabd9aa61e6deef47824fcad5d3be9c
ozgedincer <dncerozge@gmail.com>
2021-09-04 11:55:29 +0300

    ödev6

--- SQL.sql
+++ SQL.sql
@@ -77,3 +77,17 @@ SELECT * FROM customer
 WHERE store_id = 1
 ORDER BY last_name DESC
 LIMIT 4 ;
+
+
+--ÖDEV 6
+
+--film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+SELECT AVG(rental_rate) FROM film ;
+--film tablosunda bulunan filmlerden kaç tanesi 'C' karekteri ile başlar?
+SELECT COUNT('c%') FROM film ;
+--film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+SELECT MAX(length) FROM film 
+WHERE rental_rate = 0.99 ;
+--film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+SELECT COUNT(replacement_cost) FROM film 
+WHERE length > 150 ;
